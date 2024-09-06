000b33d4bce8eda9e7a6c27e89bc222c0fc8d500
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2024-09-06 02:32:15 -0500

    feat: Layout of the administrator manager.

---
+++ web/views/pages/admin/administradores/modules/listado.php
@@ -0,0 +1,44 @@
+<div class="content">
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">
+                    <a href="" class="btn bg-default templateColor py-2 px-3 btn-sm">Agregar Administrador</a>
+                </h3>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nombre</th>
+                            <th>Correo electrónico</th>
+                            <th>Rol</th>
+                            <th>Último ingreso</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Jose Tienda</td>
+                            <td>admin@ecommerce.com</td>
+                            <td>Admin</td>
+                            <td>2024-09-06 2:20:20</td>
+                            <td>
+                                <div class="btn-group">
+                                    <a href="" class="btn btn-warning border-0 mr-2 btn-sm  px-3">
+                                        <i class="fas fa-edit" style="color: white;"></i>
+                                    </a>
+                                    <a href="" class="btn btn-danger border-0 mr-2  btn-sm  px-3">
+                                        <i class="fas fa-trash-alt" style="color: white;"></i>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
